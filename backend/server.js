28ed2456d2cb1b3be294237a1a48b1834f40a8cd
Murshidmuhammedp <murshid.mmp@gmail.com>
2025-03-21 09:18:14 +0530

    origin

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ import testRouter from './routes/questionsRoutes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://texol.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', userRouter);
